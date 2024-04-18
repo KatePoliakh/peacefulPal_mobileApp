c352e483d1708ac2f595b5c77bb0e22460f56229
KatePoliakh <epolyakh.p@gmail.com>
2024-04-19 00:27:54 +0300

    auth work with server

--- src/screens/enterApp/AuthPage.js
+++ src/screens/enterApp/AuthPage.js
@@ -1,11 +1,11 @@
-import React, {useContext, useState} from 'react';
+import React, { useContext, useState } from 'react';
 import {
   SafeAreaView,
   View,
   Text,
-  TextInput,
   TouchableOpacity,
   StyleSheet,
+  Alert,
 } from 'react-native';
 import colors from '../../constants/colors';
 import { windowWidth, windowHeight } from '../../constants/dimensions';
@@ -15,145 +15,121 @@ import CustomButton from '../../components/CustomButton';
 import { AuthContext } from '../../context/AuthContext';
 import { RFValue } from 'react-native-responsive-fontsize';
 
-
-function AuthPage ({navigation}){
-  const {login} = useContext(AuthContext);
-
+function AuthPage({ navigation }) {
+  const { login } = useContext(AuthContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState(null);
-  const validateEmail = (email) => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('https://3a17-95-64-169-174.ngrok-free.app/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        // Login successful, navigate to main screen or dashboard
+        login();
+      } else {
+        // Login failed, display error message
+        const data = await response.json();
+        Alert.alert('Error', data.error);
+      }
+    } catch (error) {
+      console.error('Error logging in:', error);
+      Alert.alert('Error', 'An unexpected error occurred');
+    }
   };
 
-  const handleLogin = () => {
-    // Validate the user's input
-    if (!validateEmail(email)) {
-      alert('Please enter a valid email.');
-      return;
-    }
-    if (password === '') {
-      alert('Password cannot be empty.');
-      return;
-    }
-    login();
-  };
   return (
-    
-    <SafeAreaView style={styles.authPage}>
-      <View style={styles.BgCircle} />
-      <View style={styles.WeclomeTitle}>
+      <SafeAreaView style={styles.authPage}>
+        <View style={styles.BgCircle} />
+        <View style={styles.WeclomeTitle}>
           <Text style={styles.Title}>Welcome back</Text>
         </View>
-      <View style={styles.container}>
-        <View style={styles.centeredView}>
-          
+        <View style={styles.container}>
+          <Text style={styles.loginText}>Login</Text>
+          <InputField
+              label={'Email ID'}
+              keyboardType="email-address"
+              value={email}
+              onChangeText={setEmail}
+          />
+          <InputField
+              label={'Password'}
+              inputType="password"
+              value={password}
+              onChangeText={setPassword}
+          />
+          <CustomButton label={'Login'} onPress={handleLogin} />
+          <View style={styles.rowView}>
+            <Text>New to the app?</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('SignUpPage')}>
+              <Text style={styles.registerText}>Register</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-
-        <Text style={styles.loginText}>
-          Login
-        </Text>
-        
-      
-        <InputField
-          label={'Email ID'}
-          keyboardType="email-address"
-          value={email}
-          onChangeText={setEmail}
-        />
-
-        <InputField
-          label={'Password'}
-          inputType="password"
-          //fieldButtonLabel={"Forgot?"}
-          fieldButtonFunction={() => {}}
-          value={password}
-        />
-        
-        {errorMessage && <Text style={styles.error}>{errorMessage}</Text>}
-        <CustomButton label={"Login"} onPress={() => {login()}} />
-
-        <View style={styles.rowView}>
-          <Text >New to the app?</Text>
-          <TouchableOpacity onPress={() => navigation.navigate('SignUpPage')}>
-            <Text style={styles.registerText}> Register</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   authPage: {
-    position: "relative",
+    position: 'relative',
     flex: 1,
     backgroundColor: colors.startPageBg,
     overflow: 'hidden',
   },
   BgCircle: {
-    position: "absolute",
-    top: "30%",
-    left: "-40%",
+    position: 'absolute',
+    top: windowHeight * 0.4,
+    left: -windowWidth * 0.4,
     width: windowWidth,
-    height: windowHeight * 0.5,
-    borderRadius: (windowWidth) / 2,
+    height: windowWidth,
+    borderRadius: windowWidth / 2,
     backgroundColor: colors.StartPageCircle,
   },
   WeclomeTitle: {
-    position: "absolute",
+    position: 'absolute',
     flex: 1,
     paddingTop: '30%',
     justifyContent: 'center',
     alignItems: 'center',
   },
   Title: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
     color: colors.white,
-    fontSize: RFValue(55),
+    fontSize: RFValue(50),
     fontWeight: '700',
-    textAlign: "center",
-    lineHeight: '65%',
+    textAlign: 'center',
+    left: windowWidth * 0.01,
   },
   container: {
     paddingHorizontal: 25,
     justifyContent: 'center',
-    top: windowHeight * 0.25,
-  },
-  centeredView: {
-    alignItems: 'center'
+    top: windowHeight * 0.3,
   },
   loginText: {
     fontSize: RFValue(28),
     fontWeight: '800',
     color: colors.white,
-    marginBottom: '40%',
-  },
-  centeredText: {
-    textAlign: 'center', 
-    color: colors.white, 
-    marginBottom: 30
+    marginBottom: '10%',
   },
   rowView: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginBottom: 30,
   },
-  touchableOpacityStyle: {
-    borderColor: colors.BottomButton,
-    backgroundColor: colors.BottomButton,
-    borderWidth: windowWidth * 0.005, // 0.5% of screen width
-    borderRadius: windowWidth * 0.025, // 2.5% of screen width
-    paddingHorizontal: windowWidth * 0.075, // 7.5% of screen width
-    paddingVertical: windowHeight * 0.025, // 2.5% of screen height
-  },
   registerText: {
-    color: colors.BottomButton, 
-    fontWeight: '700'
-  }
+    color: colors.BottomButton,
+    fontWeight: '700',
+  },
 });
 
 export default AuthPage;
--- src/screens/enterApp/SignUpPage.js
+++ src/screens/enterApp/SignUpPage.js
@@ -5,7 +5,7 @@ import {
   View,
   Text,
   TouchableOpacity,
-  StyleSheet,
+  StyleSheet, Alert,
 } from 'react-native';
 
 import DatePicker from 'react-native-date-picker';
@@ -17,10 +17,38 @@ import { windowWidth, windowHeight } from '../../constants/dimensions';
 import colors from '../../constants/colors';
 
 const RegisterScreen = ({navigation}) => {
-  //const [date, setDate] = useState(new Date());
-  const [open, setOpen] = useState(false);
-  //const [dobLabel, setDobLabel] = useState('Date of Birth');
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const handleRegister = async () => {
+    // Check if passwords match
+    if (password !== confirmPassword) {
+      Alert.alert('Error', 'Passwords do not match');
+      return;
+    }
+    try {
+      const response = await fetch('https://3a17-95-64-169-174.ngrok-free.app/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ fullName, email, password }),
+      });
 
+      if (response.ok) {
+        // Registration successful, navigate to login screen
+        navigation.goBack();
+      } else {
+        // Registration failed, display error message
+        const data = await response.json();
+        Alert.alert('Error', data.error);
+      }
+    } catch (error) {
+      console.error('Error registering user:', error);
+      Alert.alert('Error', 'An unexpected error occurred');
+    }
+  };
   return (
     <SafeAreaView style={styles.signUpPage}>
       <View style={styles.BgCircle} />
@@ -31,21 +59,14 @@ const RegisterScreen = ({navigation}) => {
         showsVerticalScrollIndicator={false}
         style={styles.scrollView}>
         <View style={styles.centeredView}>
-          
         </View>
-
         <Text style={styles.registerText}>Register</Text>
+        <InputField label={'Full Name'} value={fullName} onChangeText={setFullName}/>
+        <InputField label={'Email ID'} keyboardType="email-address" value={email} onChangeText={setEmail}/>
+        <InputField label={'Password'} inputType="password" value={password} onChangeText={setPassword}/>
+        <InputField label={'Confirm Password'} inputType="password" value={confirmPassword} onChangeText={setConfirmPassword}/>
 
-        <InputField label={'Full Name'} />
-        <InputField label={'Email ID'} keyboardType="email-address" />
-        <InputField label={'Password'} inputType="password" />
-        <InputField label={'Confirm Password'} inputType="password" />
-
-        
-
-        
-
-        <CustomButton label={'Register'} onPress={() => {}} />
+        <CustomButton label={'Register'} onPress={handleRegister} />
 
         <View style={styles.loginView}>
           <Text>Already registered?</Text>
@@ -57,92 +78,92 @@ const RegisterScreen = ({navigation}) => {
     </SafeAreaView>
   );
 };
-
 const styles = StyleSheet.create({
   signUpPage: {
     flex: 1,
     backgroundColor: colors.startPageBg,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: '5%',
     overflow: 'hidden',
   },
   BgCircle: {
     position: "absolute",
-    top: "40%",
-    left: "-40%",
-    width: windowWidth, 
-    height: windowWidth, 
-    borderRadius: (windowWidth) / 2, 
+    top: windowHeight * 0.4, // Adjusted top position based on window height and width
+    left: -windowWidth * 0.4, // Adjusted left position based on window width
+    width: windowWidth,
+    height: windowWidth,
+    borderRadius: windowWidth / 2,
     backgroundColor: colors.StartPageCircle,
   },
   WeclomeTitle: {
-    position: "absolute",
-    flex: 1,
-    paddingTop: '20%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingLeft: '1%', 
+    paddingTop: '10%',
   },
   Title: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
     color: colors.white,
-    fontSize: 60,
-    lineHeight: 70,
+    fontSize: windowWidth * 0.1, // Adjusted font size based on window width
+    lineHeight: windowWidth * 0.12, // Adjusted line height based on window width
     fontWeight: '700',
     textAlign: "center",
   },
   scrollView: {
-    paddingHorizontal: 25,
+    width: '100%', // Ensures the scrollView takes full width
+    paddingHorizontal: '5%', // Adjusted padding based on percentage
   },
   centeredView: {
     alignItems: 'center',
+    width: '100%', // Ensures the centered view takes full width
   },
   registerText: {
-    fontFamily: 'Roboto-Medium',
-    fontSize: 28,
-    fontWeight: '500',
+    fontSize: windowWidth * 0.07, // Adjusted font size based on window width
+    marginBottom: '5%', // Adjusted margin based on percentage
+    marginTop: '15%', // Adjusted margin based on percentage
     color: colors.white,
-    marginBottom: '10%',
-    marginTop: '65%',
+    fontWeight: '500',
   },
   socialMediaView: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginBottom: 30,
+    marginBottom: '5%', // Adjusted margin based on percentage
+    width: '100%', // Ensures the social media view takes full width
   },
   socialMediaButton: {
     borderColor: '#ddd',
     borderWidth: 2,
     borderRadius: 10,
-    paddingHorizontal: 30,
-    paddingVertical: 10,
+    paddingHorizontal: '5%', // Adjusted padding based on percentage
+    paddingVertical: '2%', // Adjusted padding based on percentage
   },
   centeredText: {
     textAlign: 'center',
     color: colors.BottomButton,
-    marginBottom: 30,
+    marginBottom: '5%', // Adjusted margin based on percentage
+    width: '100%', // Ensures the text takes full width
   },
   datePickerView: {
     flexDirection: 'row',
     borderBottomColor: colors.BottomButton,
     borderBottomWidth: 1,
-    paddingBottom: 8,
-    marginBottom: 30,
+    paddingBottom: '2%', // Adjusted padding based on percentage
+    marginBottom: '5%', // Adjusted margin based on percentage
+    width: '100%', // Ensures the date picker view takes full width
   },
   datePickerText: {
     color: '#666',
-    marginLeft: 5,
-    marginTop: 5,
+    marginLeft: '1%', // Adjusted margin based on percentage
+    marginTop: '1%', // Adjusted margin based on percentage
   },
   loginView: {
     flexDirection: 'row',
     justifyContent: 'center',
-    marginBottom: 30,
+    marginBottom: '5%', // Adjusted margin based on percentage
+    width: '100%', // Ensures the login view takes full width
   },
   loginText: {
     color: colors.BottomButton,
     fontWeight: '700',
   },
 });
+
 
 export default RegisterScreen;
