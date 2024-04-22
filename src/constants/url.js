7915454705208b853b4d07d60b1261cf4d99da10
KatePoliakh <epolyakh.p@gmail.com>
2024-04-22 18:29:10 +0300

    packages renewed + url updated

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,3 +1,5 @@
-//export const apiUrl = 'https://3a17-95-64-169-174.ngrok-free.app'; //home
+export const apiUrl = 'https://3dc4-95-64-169-174.ngrok-free.app'; //home
 
-export const apiUrl = 'https://5ce6-89-175-18-189.ngrok-free.app'; //hse
+//export const apiUrl = 'https://95.64.169.174';
+
+//export const apiUrl = 'https://5ce6-89-175-18-189.ngrok-free.app'; //hse
